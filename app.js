b64a11c4a6e9cc8e51afdbc312fd4fb6c8707bc9
birupakhya <birupakhyad@mail.usf.edu>
2017-04-25 19:12:27 -0400

    set port to 3000

--- app.js
+++ app.js
@@ -35,7 +35,9 @@ app.set('view engine', '.hbs');
 //     message: 'This is a test. Yes, just a test.'
 //   });
 // });
-console.log('Express server listening on port ' + app.get('port'));
+//console.log('Express server listening on port ' + app.get('port'));
+
+app.set('port', process.env.PORT || 3000);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
