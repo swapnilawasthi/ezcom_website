986d3314098c238f2b51efd1686a68bc4d65d2f0
birupakhya <birupakhyad@mail.usf.edu>
2017-04-25 17:38:15 -0400

    Added mongodb database, automatic retrieval of products from the database

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
 var express = require('express');
+var http = require('http');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cons = require('consolidate');
+var expressHbs = require('express-handlebars');
+var mongoose = require('mongoose');
+
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
 
 var app = express();
 
+//mongoose.connect('localhost:27017/ezcomdb_master');
+mongoose.connect('mongodb://bigdatadb:JlcX7j3VBNZl8d0BUYQW0Q9gSKD9SW0PgMQ13MsrpAApSWh6kzlgUDwILKbBeNR8l451KWKu1FKHpn8vQ4HKiw==@bigdatadb.documents.azure.com:10250/ezcomdb_master/?ssl=true');
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// // view engine setup
+// app.engine('hbs', cons.handlebars);
+// app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
+
+// app.get('/', function(res, res) {
+//   res.render('index', {
+//     title: 'EZCOM e-Commerce',
+//     author: {name: 'Birupakhya', age:29},
+//     message: 'This is a test. Yes, just a test.'
+//   });
+// });
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +48,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+//app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ seed/product-seeder.js
@@ -0,0 +1,65 @@
+var Product = require('../models/product');
+
+var mongoose = require('mongoose');
+
+mongoose.connect('localhost:27017/ezcomdb_master');
+
+
+var products = [
+    new Product({
+    imagePath: 'http://s7d2.scene7.com/is/image/SamsungUS/S8Plus_Silver_Front_032317?$product-details-jpg$',
+    title: 'Samsung Galaxy 8',
+    description: 'Samsung Galaxy 8',
+    price: 679
+    }),
+    new Product({
+    imagePath: 'http://cdn2.gsmarena.com/vv/pics/apple/apple-iphone-7-3.jpg',
+    title: 'iPhone 8 Gold',
+    description: 'iPhone 8 Gold',
+    price: 569
+    }),
+    new Product({
+    imagePath: 'https://images-na.ssl-images-amazon.com/images/I/91NtH6M1OmL._SL1500_.jpg',
+    title: 'HTC One M8',
+    description: 'HTC One M8',
+    price: 369
+    }),
+    new Product({
+    imagePath: 'http://s7d2.scene7.com/is/image/SamsungUS/S8Plus_Silver_Front_032317?$product-details-jpg$',
+    title: 'Samsung Galaxy 8',
+    description: 'Samsung Galaxy 8',
+    price: 10
+    }),
+    new Product({
+    imagePath: 'http://s7d2.scene7.com/is/image/SamsungUS/S8Plus_Silver_Front_032317?$product-details-jpg$',
+    title: 'Samsung Galaxy 8',
+    description: 'Samsung Galaxy 8',
+    price: 10
+    }),
+    new Product({
+    imagePath: 'http://s7d2.scene7.com/is/image/SamsungUS/S8Plus_Silver_Front_032317?$product-details-jpg$',
+    title: 'Samsung Galaxy 8',
+    description: 'Samsung Galaxy 8',
+    price: 10
+    }),
+    new Product({
+    imagePath: 'http://s7d2.scene7.com/is/image/SamsungUS/S8Plus_Silver_Front_032317?$product-details-jpg$',
+    title: 'Samsung Galaxy 8',
+    description: 'Samsung Galaxy 8',
+    price: 10
+    })
+];
+
+var done = 0;
+for (var i = 0; i < products.length; i++){
+    products[i].save(function(err, result){
+        done++;
+        if (done === products.length){
+            exit();
+        }
+    });
+}
+
+function exit(){
+    mongoose.disconnect();
+}
