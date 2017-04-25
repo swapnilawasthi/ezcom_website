8e14addfa36cc19e5965ba4936cd0e7da1c1ba15
birupakhya <birupakhyad@mail.usf.edu>
2017-04-25 19:20:29 -0400

    tweaked port

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.set('view engine', '.hbs');
 // });
 //console.log('Express server listening on port ' + app.get('port'));
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
